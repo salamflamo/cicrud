00bcb1ce4652aa2f5ed0ad6b901d99984b387ca8
salamflamo <untungsuprapto69@gmail.com>
2017-10-11 17:30:01 +0700

    Belajar membuat email aktivasi

---
+++ application/controllers/Sendmail.php
@@ -0,0 +1,40 @@
+<?php
+
+class Sendmail extends CI_Controller
+{
+
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('string');
+
+  }
+
+  public function send($email,$token)
+  {
+
+    $config['mailtype'] = 'html';
+
+    $this->load->library('email',$config);
+    $this->email->set_newline("\r\n");
+    $this->email->from('Email Perusahaan','Salamuddin');
+    $this->email->to($email);
+    $this->email->subject('Njajal lagi');
+    $this->email->message('<a href="'.base_url().$email.'/'.$token.'">Token ini </a>');
+    if ($this->email->send()) {
+      echo "Berhasil kirim";
+    } else {
+      echo "Gagal jhon";
+    }
+  }
+
+  public function string()
+  {
+    echo random_string('alnum',16);
+  }
+
+  public function cektoken($token)
+  {
+    echo $token;
+  }
+}
